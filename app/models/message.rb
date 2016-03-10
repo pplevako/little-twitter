1ccdd8c9f5dc67c50d920183bd3637a64576d13e
Pavel Plevako <kaminapoke@gmail.com>
2016-03-10 22:38:05 +0300

    Add Like model and like! method to Message.
    Add uniqueness validation to Like model.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,10 @@
 class Message < ActiveRecord::Base
   belongs_to :user
+  has_many :likes, as: :likeable
 
   validates :content, length: {maximum: 140}
+
+  def like!(user)
+    self.likes.create(user: user)
+  end
 end
