aca40fedfae3fb4e2a0203c7220a96bad469e2a9
Pavel Plevako <kaminapoke@gmail.com>
2016-03-10 23:13:02 +0300

    Add like route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :messages
+      resources :messages do
+        patch :like, on: :member
+      end
     end
   end
   scope :api do
