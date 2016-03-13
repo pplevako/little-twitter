4899a84383381232f1f7eeaa7fa20ced54359805
Pavel Plevako <kaminapoke@gmail.com>
2016-03-13 18:10:19 +0300

    Add statistics page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,16 @@ class User < ActiveRecord::Base
 
   has_many :messages, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def self.top_by_messages_count(time_interval='all', limit=5)
+    users = User.select('users.*, COUNT(messages.id) AS messages_count')
+                .joins(:messages)
+                .group('users.id')
+                .order('messages_count DESC')
+                .limit(limit)
+
+    users = users.where('messages.created_at > ?', 1.day.ago) if time_interval == 'day'
+    users = users.where('messages.created_at > ?', 1.week.ago) if time_interval == 'week'
+    return users
+  end
 end
