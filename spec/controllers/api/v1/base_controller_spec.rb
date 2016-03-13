59c70d1cdb010c5905509ec5f674bfa531b1e6ce
Pavel Plevako <kaminapoke@gmail.com>
2016-03-13 23:42:55 +0300

    More specs.

---
+++ spec/controllers/api/v1/base_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::BaseController, type: :controller do
+  controller do
+    def index
+      raise ActiveRecord::RecordNotFound
+    end
+  end
+
+  before do
+    get :index
+  end
+
+  it { is_expected.to respond_with(:not_found) }
+  # TODO check json contents
+end
