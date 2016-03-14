e79ec1659204af3d5d17832516f505db65ff99c3
Pavel Plevako <kaminapoke@gmail.com>
2016-03-14 22:16:36 +0300

    Add index to messages created_at.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313191050) do
+ActiveRecord::Schema.define(version: 20160314185600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160313191050) do
     t.integer  "likes_count", default: 0
   end
 
+  add_index "messages", ["created_at"], name: "index_messages_on_created_at", using: :btree
   add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
