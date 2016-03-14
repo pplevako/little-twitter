00dbc7962b426489c79bd2318f489115c9bdf3bf
Pavel Plevako <kaminapoke@gmail.com>
2016-03-14 14:11:24 +0300

    Add another top-5 query.

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,12 +1,13 @@
 class StatisticsController < ApplicationController
   def show
-    @time_intervals = [[t('.all_time'), :all], [t('.day'), :day], [t('.week'), :week]]
+    @time = params[:time] || :all
+    @users_by_messages = User.by_messages_count(@time).limit(5)
 
-    @selected_interval = params[:time_interval] || :all
-    @users = User.top_by_messages_count(@selected_interval)
+    @users_by_likes = User.by_likes_count(@time).limit(5)
+    @messages_by_likes = Message.where(user_id: @users_by_likes.map(&:id)).order(likes_count: :desc)
   end
 
   def update
-    redirect_to statistics_url(time_interval: params[:time_interval])
+    redirect_to statistics_url(time: params[:time])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,25 @@ class User < ActiveRecord::Base
 
   has_many :messages, dependent: :destroy
 
-  def self.top_by_messages_count(time_interval='all', limit=5)
+  def self.by_messages_count(time)
     users = User.select('users.*, COUNT(messages.id) AS messages_count')
                 .joins(:messages)
                 .group('users.id')
                 .order('messages_count DESC')
-                .limit(limit)
 
-    users = users.where('messages.created_at > ?', 1.day.ago) if time_interval == 'day'
-    users = users.where('messages.created_at > ?', 1.week.ago) if time_interval == 'week'
+    users = users.where('messages.created_at > ?', 1.day.ago) if time == 'day'
+    users = users.where('messages.created_at > ?', 1.week.ago) if time == 'week'
+    return users
+  end
+
+  def self.by_likes_count(time)
+    users = User.select('users.*, MAX(messages.likes_count) AS max_likes_count')
+                .joins(:messages)
+                .group('users.id')
+                .order('max_likes_count DESC')
+
+    users = users.where('messages.created_at > ?', 1.day.ago) if time == 'day'
+    users = users.where('messages.created_at > ?', 1.week.ago) if time == 'week'
     return users
   end
 end
