c1759f3b3cf731da4dbbbb68c309108e7ecb654d
Pavel Plevako <kaminapoke@gmail.com>
2016-03-14 18:11:44 +0300

    Add some StatisticsController specs.

--- spec/controllers/statistics_controller_spec.rb
+++ spec/controllers/statistics_controller_spec.rb
@@ -1,4 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe StatisticsController, type: :controller do
+  describe 'GET show' do
+    it 'assigns @users_by_messages' do
+      @user1 = FactoryGirl.create(:user)
+      @user2 = FactoryGirl.create(:user)
+      FactoryGirl.create(:message, user: @user1)
+      FactoryGirl.create(:message, user: @user2, created_at: 2.days.ago)
+
+      get :show, time: 'day'
+      expect(assigns(:users_by_messages)).to eq([@user1])
+    end
+  end
+
+  describe 'PATCH update' do
+    it 'redirects to statistics url' do
+      patch :update, time: 'week'
+      is_expected.to redirect_to(statistics_url(time: 'week'))
+    end
+  end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Message, type: :model do
   describe '#like!' do
     let(:message) { FactoryGirl.create(:message) }
     let(:user) { FactoryGirl.create(:user) }
+
     it 'adds like to a message' do
       expect { message.like!(user) }.to change { message.reload.likes_count }.from(0).to(1)
     end
