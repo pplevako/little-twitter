aec82e77aab6f0bbecf425106547d42cd200840d
Pavel Plevako <kaminapoke@gmail.com>
2016-03-15 13:47:10 +0300

    Add seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user1 = FactoryGirl.create(:user)
+user2 = FactoryGirl.create(:user)
+user3 = FactoryGirl.create(:user)
+
+message1 = FactoryGirl.create(:message, user: user1, created_at: 1.days.ago)
+message2 = FactoryGirl.create(:message, user: user1, created_at: 1.days.ago)
+message3 = FactoryGirl.create(:message, user: user1)
+
+message4 = FactoryGirl.create(:message, user: user3)
+message5 = FactoryGirl.create(:message, user: user3)
+
+FactoryGirl.create(:like, likeable: message1)
+FactoryGirl.create(:like, likeable: message2)
+
+FactoryGirl.create(:like, likeable: message4)
+FactoryGirl.create(:like, likeable: message5)
+FactoryGirl.create(:like, likeable: message5)
