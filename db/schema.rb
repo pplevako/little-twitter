172901876e7bc8cb25a668d287438bf65d16118a
Pavel Plevako <kaminapoke@gmail.com>
2016-03-21 23:29:10 +0300

    Add null database constraint to message's user_id.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160314185600) do
+ActiveRecord::Schema.define(version: 20160321200413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20160314185600) do
   add_index "likes", ["user_id"], name: "index_likes_on_user_id", using: :btree
 
   create_table "messages", force: :cascade do |t|
-    t.integer  "user_id"
+    t.integer  "user_id",                 null: false
     t.string   "content"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
