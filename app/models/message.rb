5af190997ed8cde62a3f80341f21c8cf4f4a6c46
Pavel Plevako <kaminapoke@gmail.com>
2016-03-21 21:59:22 +0300

    Add message's content presence validation.

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ActiveRecord::Base
   belongs_to :user
   has_many :likes, as: :likeable, dependent: :destroy
 
-  validates :content, length: { maximum: 140 }
+  validates :content, length: { maximum: 140 }, presence: true
 
   def like!(user)
     likes.create(user: user)
