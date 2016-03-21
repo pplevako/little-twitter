9800cab8d76e83b1cd2e0585f3fb1104eca56cda
Pavel Plevako <kaminapoke@gmail.com>
2016-03-21 23:44:17 +0300

    Add null database constraint to like's user_id.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160321200413) do
+ActiveRecord::Schema.define(version: 20160321204137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20160321200413) do
   create_table "likes", force: :cascade do |t|
     t.integer  "likeable_id"
     t.string   "likeable_type"
-    t.integer  "user_id"
+    t.integer  "user_id",       null: false
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
