6abb21144385daa50b884cd5d14467ac54cef2cd
Pavel Plevako <kaminapoke@gmail.com>
2016-03-22 14:48:01 +0300

    Additional message specs.

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -13,4 +13,6 @@ RSpec.describe Message, type: :model do
   it { is_expected.to validate_presence_of(:content) }
 
   it { is_expected.to validate_presence_of(:user) }
+
+  it { is_expected.to validate_length_of(:content).is_at_most(140) }
 end
